fb36830db685e16931a733210b55c9bbc2b15dfd
AngelCas20 <angelcastillo2060@gmail.com>
2025-09-07 00:01:36 -0500

    update descriptives

--- 06_visual/script/01_descriptives.R
+++ 06_visual/script/01_descriptives.R
@@ -43,12 +43,11 @@ P_90_10 = function(x) {round(P90(x = x)/P10(x = x),2)}
 Skewness = function(x) skewness(x)
 
 ### Compute stats for table
-table_continuous = datasummary(formula = All(table_continuous) ~ N + Mean + SD + P10 + Median + P90 + P_90_10 + Skewness + Correlation,
+table_continuous = datasummary(formula = All(table_continuous) ~ Mean + SD + P10 + Median + P90 + P_90_10 + Skewness + Correlation,
                                data = table_continuous,
                                output = 'data.frame',fmt = function(x) format(round(x,2),big.mark = ',')) %>%
                                arrange(desc(Mean)) %>% 
-                               mutate(P_90_10 = ifelse(P_90_10 == 7.29,P_90_10,' '),
-                                      N = format(N,big.mark = ',',dec.mark = '.'))
+                               mutate(P_90_10 = ifelse(P_90_10 == 7.29,P_90_10,' '))
 ### Rename stats
 table_continuous = table_continuous %>% 
                    rename('Promedio' = "Mean",
@@ -135,21 +134,26 @@ table_categorical = table_categorical %>%
 ##==: 3. Add table title and footnote
 
 table_categorical = table_categorical %>% 
-                    tab_header(title = 'Distribución de las variables categóricas') %>% 
-                    tab_footnote(footnote = 'Esta tabla muestra la cantidad de observaciones en cada nivel de cada variable, 
-                                             así como la proprción que representan del total de distribución.
-                                             Las columnas de Promedio y SD, muestran respectivamente, el promedio y la distribución estándar
-                                             de la variable dependiente para cada uno de los niveles de las covariables. 
-                                             El tamaño de la muestra es de 14,632 observaciones.')
+                    tab_header(title = 'Distribución de las variables categóricas') 
+
+# ##%>% 
+#                     tab_footnote(footnote = 'Esta tabla muestra la cantidad de observaciones en cada nivel de cada variable, 
+#                                              así como la proprción que representan del total de distribución.
+#                                              Las columnas de Promedio y SD, muestran respectivamente, el promedio y la distribución estándar
+#                                              de la variable dependiente para cada uno de los niveles de las covariables. 
+#                                              El tamaño de la muestra es de 14,632 observaciones.')
 
 table_continuous = table_continuous %>% 
-                   tab_header(title = 'Distribución de las variables númericas') %>% 
-                   tab_footnote(footnote = 'Esta tabla muestra un conjunto de estadísticas descriptivas de las variables continuas incluidas en el conjunto de datos.
-                                            Las columnas de Promedio y SD muestran la desviación estándar de las variables. 
-                                            Mientras que las columnas P10, Mediana y P90 muestran los valores de la distribución en los percentiles 10, 50 y 90.
-                                            La columna Razón P90/P10 muestra el cociente entre los percentiles 90 y 10.
-                                            La columna Asimetría muestra el coeficiente de asimetría de la distribución,
-                                            Finalmente la columna Correlación muestra el valor de el Coeficiente de Correlación de Pearson con la variable de Ingresos laborales por hora.')
+                   tab_header(title = 'Distribución de las variables númericas')
+
+# %>% 
+##                   tab_footnote(footnote = 'Esta tabla muestra un conjunto de estadísticas descriptivas de las variables continuas incluidas en el conjunto de datos.
+#                                            Las columnas de Promedio y SD muestran la desviación estándar de las variables. 
+#                                            Mientras que las columnas P10, Mediana y P90 muestran los valores de la distribución en los percentiles 10, 50 y 90.
+#                                            La columna Razón P90/P10 muestra el cociente entre los percentiles 90 y 10.
+#                                            La columna Asimetría muestra el coeficiente de asimetría de la distribución,
+#                                            Finalmente la columna Correlación muestra el valor de el Coeficiente de Correlación de Pearson con la variable de Ingresos laborales por hora.
+#                                            El tamaño de la muestra es de 14,632 observaciones.')
 
 ##==: 4. Export tables
 table_continuous %>% as_latex() %>% write_lines(x = .,file = '06_visual/output/01_tabla_continuas.tex')
@@ -160,19 +164,42 @@ latex_table_continuous = read_lines('06_visual/output/01_tabla_continuas.tex')
 latex_table_categoricas = read_lines('06_visual/output/01_tabla_categoricas.tex')
 
 ### Subset tables
-latex_table_continuous = latex_table_continuous[3:25]
-latex_table_categoricas = latex_table_categoricas[3:70]
+latex_table_continuous = latex_table_continuous[3:16]
+latex_table_categoricas = latex_table_categoricas[3:62]
 
 ### Append tables
 latex_table_continuous = append(c("\\centering \\footnotesize \\setlength{\\tabcolsep}{4pt} \\renewcommand{\\arraystretch}{0.8}",
                                   "\\begingroup",
-                                  "\\fontsize{9.0pt}{8.5pt}\\selectfont"),
+                                  "\\fontsize{7.0pt}{8.5pt}\\selectfont"),
                                 latex_table_continuous)
 
-latex_table_continuous = append(c("\\centering \\footnotesize \\setlength{\\tabcolsep}{4pt} \\renewcommand{\\arraystretch}{0.8}",
+latex_table_continuous = append(latex_table_continuous,
+                                c('\\begin{minipage}{0.8\\textwidth}
+                                   \\begin{tablenotes}[scriptsize,flushleft]
+                                   \\item \\footnotesize \\textit{Nota:} Esta tabla muestra un conjunto de estadísticas descriptivas de las variables continuas incluidas en el conjunto de datos.
+                                   Las columnas de Promedio y SD muestran la desviación estándar de las variables.Mientras que las columnas P10, Mediana y P90 muestran los valores de la distribución en los percentiles 10, 50 y 90.
+                                   La columna Razón P90/P10 muestra el cociente entre los percentiles 90 y 10.
+                                   La columna Asimetría muestra el coeficiente de asimetría de la distribución.
+                                   Finalmente la columna Correlación muestra el valor de el Coeficiente de Correlación de Pearson con la variable de Ingresos laborales por hora.
+                                   El tamaño de la muestra es de 14,632 observaciones.
+                                   \\end{tablenotes}
+                                   \\end{minipage}'))
+
+latex_table_categoricas = append(c("\\centering \\footnotesize \\setlength{\\tabcolsep}{4pt} \\renewcommand{\\arraystretch}{0.8}",
                                   "\\begingroup",
-                                  "\\fontsize{9.0pt}{8.5pt}\\selectfont"),
-                                latex_table_categoricas)
+                                  "\\fontsize{7.0pt}{8.5pt}\\selectfont"),
+                                 latex_table_categoricas)
+
+latex_table_categoricas = append(latex_table_categoricas,
+                                 c('\\begin{minipage}{0.8\\textwidth}
+                                   \\begin{tablenotes}[scriptsize,flushleft]
+                                   \\item \\footnotesize \\textit{Nota:} Esta tabla muestra la cantidad de observaciones en cada nivel de cada variable, 
+                                   así como la proprción que representan del total de distribución.
+                                   Las columnas de Promedio y SD, muestran respectivamente, el promedio y la distribución estándar
+                                   de la variable dependiente para cada uno de los niveles de las covariables. 
+                                   El tamaño de la muestra es de 14,632 observaciones.
+                                   \\end{tablenotes}
+                                   \\end{minipage}'))
 
 ##==: 6. Reexport tables
 latex_table_continuous %>% write_lines(x = .,file = '06_visual/output/01_tabla_continuas.tex')
